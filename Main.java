786c6b9056e0ffff45a69a7457c25a7cfc45a8b8
davis eban <126353156+eban20@users.noreply.github.com>
2023-02-25 19:10:45 +0000

    height must be in meters and not cm

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+
+ Scanner plex = new Scanner(System.in);
+
+    System.out.printf("what is your weight in kg (kilogram) :" );
+    Double weight = plex.nextDouble();
+    //plex is variable for the Scanner input 
+    plex.nextDouble(); 
+
+    System.out.printf(" what is your height in m (meters) ");
+    
+  }
+}
