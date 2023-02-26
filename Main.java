e2a5d605da2eff91782cd6ba7959b5be16965a7a
davis eban <126353156+eban20@users.noreply.github.com>
2023-02-26 09:44:24 +0000

    completed BMI calculator

--- Main.java
+++ Main.java
@@ -5,12 +5,18 @@ class Main {
 
  Scanner plex = new Scanner(System.in);
 
-    System.out.printf("what is your weight in kg (kilogram) :" );
-    Double weight = plex.nextDouble();
+    System.out.printf("what is your weight in kg (kilogram) :");
+    float weight = plex.nextFloat();
     //plex is variable for the Scanner input 
-    plex.nextDouble(); 
+    plex.nextLine(); 
 
-    System.out.printf(" what is your height in m (meters) ");
+    System.out.printf(" what is your height in m (meters) :");
+    float height = plex.nextFloat();
+    plex.nextLine();
+
+    float BMI = weight / (height * height);
+    System.out.printf("your BMI is " + BMI + " kgm2");
     
+    plex.close();
   }
 }
